2c888f7e7574c26f1e8dbefd1ac3385a49089ac8
tawahinBeirut <gheribij59@gmail.com>
2024-03-22 11:40:10 +0100

    Debut route Pour recuperer qu'un seul sujet #8

---
+++ opimiel-backend/src/app/api/subjects/[...id]/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse} from "next/server";
+import Prisma from "@/utils/PrismaClient";
+
+export async function GET(req:NextRequest,params:{params : {id:string}}) {
+
+    const id= params.params.id[0];
+    
+    return NextResponse.json({data: id})
+}   
