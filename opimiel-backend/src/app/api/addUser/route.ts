763c4afad59d5e9c2a8275c3bd1edac3dfd29ad3
tawahinBeirut <gheribij59@gmail.com>
2024-03-22 10:38:14 +0100

    Premiere version sans le truc firebase pour ajouter un user #2

---
+++ opimiel-backend/src/app/api/addUser/route.ts
@@ -0,0 +1,30 @@
+
+import Prisma from "@/utils/PrismaClient"
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest) {
+
+    let email;
+    let password;
+    let name;
+    try{
+    const body = await req.json();
+    email = body.email;
+    password = body.password;
+    name = body.name;
+    }catch(e){
+        return NextResponse.json({pe:"o",statusbar:403})
+    }   
+
+    if (typeof email == "string" && typeof password == "string" && typeof name == "string"){
+    await Prisma.user.create({
+        data:{
+            email: email,
+            name: name,
+            password: password
+        }
+    })
+    return NextResponse.json({oe:"oe",statusbar:200})
+}
+    else return NextResponse.json({oe:"nan",statusbar: 378})
+}
