449e1fb0e36abb3684a851e3bdacc39079a8ce15
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2024-03-27 22:22:29 +0100

    backend next12

---
+++ opimiel-backend/src/pages/api/getFavorites/[...id].ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Prisma from "@/utils/PrismaClient";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const { id } = await req.query;
+    if (!id) {
+      res.status(400).json({ error: "L'id n'es pas spécifié !" });
+      return;
+    }
+    const response = await Prisma.favorite.findMany({
+      where: {
+        userId: id as string,
+      },
+    });
+    if (!response) {
+      res.status(400).json({ error: "L'id n'existe pas !" });
+      return;
+    }
+
+    let dataResult = [];
+    for (let i = 0; i < response.length; i++) {
+      const element = response[i];
+
+      let tempRes = await Prisma.subject.findUnique({
+        where: {
+          id: element.subjectId,
+        },
+      });
+      if (tempRes) {
+        dataResult.push(tempRes);
+      }
+    }
+
+    res.status(200).json({ message: "oe", data: dataResult });
+  }else{
+    res.status(400).json({error: "ONLY GET ARE AUTHORIZED !"})
+  }
+}
