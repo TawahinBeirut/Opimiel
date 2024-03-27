3b58dc06de033d6c48311e8506f13a74888f27e0
tawahinBeirut <gheribij59@gmail.com>
2024-03-27 22:18:22 +0100

    post request avancement

--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/model/apiCalls/ApiService.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/model/apiCalls/ApiService.kt
@@ -1,13 +1,16 @@
 import com.example.opimiel_frontend.model.apiCalls.MessageResponse
 import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService{
     @GET("subjects") // endpoint
     fun getSubjects(): Call<SubjectsResponse>
 
+    @Headers("Content-Type: application/json")
     @POST("postSubject")
-    fun postSubject(authorId:String,name:String) : Response<MessageResponse>
+    fun postSubject(@Body requestBody: PostSubjectRequest): Call<MessageResponse>
 }
