a5a9e5da7be1473a1a7279bfa9b23f5216ae525f
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2024-03-28 19:52:02 +0100

    login page

--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
@@ -4,21 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class ConnexionPage : AppCompatActivity()  {
-    private lateinit var buttonStart: Button;
+    private lateinit var buttonLogin: Button;
+    private lateinit var usernameInput: TextInputEditText;
+    private lateinit var passwordInput: TextInputEditText;
     override fun onCreate(savedInstanceState: Bundle?) {
-
         // Fais la connexion
         super.onCreate(savedInstanceState)
         setContentView(R.layout.connexion_page);
-        buttonStart = findViewById<Button>(R.id.button);
-        buttonStart.setOnClickListener{
+        buttonLogin = findViewById(R.id.login_button);
+        usernameInput = findViewById(R.id.login_username);
+        passwordInput = findViewById(R.id.login_password);
+
+        /*buttonLogin.setOnClickListener{
             var intent: Intent = Intent(this,MainActivity::class.java);
 
             // Vrai Id d'utilisateur
             intent.putExtra("UserId","b898a640-2b5f-4565-b58d-06899d774c66");
             startActivity(intent);
         }
+        */
     }
 }
