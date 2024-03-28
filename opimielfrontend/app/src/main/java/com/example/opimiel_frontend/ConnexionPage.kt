14db38a4077d14c9d006549b3e367c051beb739e
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2024-03-28 21:29:17 +0100

    test connection

--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
@@ -1,19 +1,50 @@
 package com.example.opimiel_frontend
 
+import ApiService
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.opimiel_frontend.databinding.ActivityMainBinding
+import com.example.opimiel_frontend.databinding.ConnexionPageBinding
+import com.example.opimiel_frontend.model.apiCalls.AddUserResponse
+import com.example.opimiel_frontend.model.apiCalls.MessageResponse
+import com.example.opimiel_frontend.model.apiCalls.PostAddUserRequest
+import com.example.opimiel_frontend.model.apiCalls.PostGetUser
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class ConnexionPage : AppCompatActivity()  {
     private lateinit var buttonLogin: Button;
     private lateinit var buttonRegister: Button;
     private lateinit var usernameInput: TextInputEditText;
     private lateinit var passwordInput: TextInputEditText;
+    private lateinit var binding: ConnexionPageBinding;
+
+
+    val logging = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://opimiel.vercel.app/api/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    val apiService = retrofit.create(ApiService::class.java);
 
     companion object{
         lateinit var auth: FirebaseAuth
@@ -22,6 +53,7 @@ class ConnexionPage : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Fais la connexion
         super.onCreate(savedInstanceState)
+        binding = ConnexionPageBinding.inflate(layoutInflater)
 
         auth = FirebaseAuth.getInstance()
         setContentView(R.layout.connexion_page);
@@ -38,11 +70,42 @@ class ConnexionPage : AppCompatActivity()  {
                         if(it.isSuccessful){
                             Toast.makeText(this, "Login in successfully !", Toast.LENGTH_SHORT).show()
 
-                            var intent: Intent = Intent(this,MainActivity::class.java);
 
-                            // Vrai Id d'utilisateur
-                            intent.putExtra("email", email);
-                            startActivity(intent);
+                            try {
+
+                                val requestBody = PostGetUser(email)
+                                val call = apiService.getUser(requestBody);
+
+                                call.enqueue(object : Callback<AddUserResponse> {
+                                    override fun onResponse(
+                                        call: Call<AddUserResponse>,
+                                        response: Response<AddUserResponse>
+                                    ) {
+                                        if (response.isSuccessful){
+                                            Snackbar.make(binding.root.rootView, "Requête réussie", Snackbar.LENGTH_LONG).show()
+
+                                            val userid = response.body()?.userid;
+                                            Log.d("reussite requete",response.toString())
+                                            var intent: Intent = Intent(binding.root.context,MainActivity::class.java);
+                                            // Vrai Id d'utilisateur
+                                            intent.putExtra("UserId",userid);
+                                            startActivity(intent);
+
+                                        }else{
+                                            Log.d("erreur requete",response.toString())
+                                        }
+                                    }
+
+                                    override fun onFailure(call: Call<AddUserResponse>, t: Throwable) {
+                                        TODO("Not yet implemented")
+                                    }
+
+                                })
+
+
+                            }catch (e: Exception) {
+                                Log.d("Network exception",e.toString())
+                            }
                         }
                     }).addOnFailureListener({
                         Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
@@ -58,6 +121,44 @@ class ConnexionPage : AppCompatActivity()  {
                 auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener({
                     if(it.isSuccessful){
                         Toast.makeText(this, "Account created Successfully !", Toast.LENGTH_SHORT).show()
+
+                        try {
+
+                            val requestBody = PostAddUserRequest("", email, password)
+                            val call = apiService.addUser(requestBody);
+
+                            call.enqueue(object : Callback<AddUserResponse> {
+                                override fun onResponse(
+                                    call: Call<AddUserResponse>,
+                                    response: Response<AddUserResponse>
+                                ) {
+                                    if (response.isSuccessful){
+                                        Snackbar.make(binding.root.rootView, "Requête réussie", Snackbar.LENGTH_LONG).show()
+
+                                        val userid = response.body()?.userid;
+                                        Log.d("reussite requete",response.toString())
+                                        var intent: Intent = Intent(binding.root.context,MainActivity::class.java);
+                                        // Vrai Id d'utilisateur
+                                        intent.putExtra("UserId",userid);
+                                        startActivity(intent);
+
+                                    }else{
+                                        Log.d("erreur requete",response.toString())
+                                    }
+                                }
+
+                                override fun onFailure(call: Call<AddUserResponse>, t: Throwable) {
+                                    TODO("Not yet implemented")
+                                }
+
+                            })
+
+
+                        }catch (e: Exception) {
+                            Log.d("Network exception",e.toString())
+                        }
+
+
                     }
                 }).addOnFailureListener({
                     Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/model/apiCalls/ApiService.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/model/apiCalls/ApiService.kt
@@ -1,4 +1,7 @@
+import com.example.opimiel_frontend.model.apiCalls.AddUserResponse
 import com.example.opimiel_frontend.model.apiCalls.MessageResponse
+import com.example.opimiel_frontend.model.apiCalls.PostAddUserRequest
+import com.example.opimiel_frontend.model.apiCalls.PostGetUser
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
@@ -15,6 +18,14 @@ interface ApiService{
     @Headers("Content-Type: application/json")
     @POST("postSubject")
     fun postSubject(@Body requestBody: PostSubjectRequest): Call<MessageResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("addUser")
+    fun addUser(@Body requestBody: PostAddUserRequest): Call<AddUserResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("getAccount")
+    fun getUser(@Body requestBody: PostGetUser): Call<AddUserResponse>
 
     @GET("getFavorites/{id}")
     fun getFavorites(@Path(value = "id",encoded = false) id:String): Call<SubjectsResponse>
