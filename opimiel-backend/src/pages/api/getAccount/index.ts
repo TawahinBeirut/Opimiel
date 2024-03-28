c2d8ea88a069d8c5ebaa2e309c870ce33bee78e0
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2024-03-28 20:52:18 +0100

    add backend route

---
+++ opimiel-backend/src/pages/api/getAccount/index.ts
@@ -0,0 +1,26 @@
+import Prisma from "@/utils/PrismaClient";
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    if(req.method === "POST"){
+        const {email} = req.body;
+
+        if(!email){
+            res.status(400).json({error: "Email not specified !"})
+            return;
+        }
+        const account = await Prisma.user.findUnique({
+            where:{
+                email: email
+            }
+        })
+        if(!account){
+            res.status(404).json({error: "User not found !"})
+            return;
+        }
+        res.status(200).json({userid: account.id})
+    }else{
+        res.status(400).json({error: "THIS ROUTE ONLY ACCEPT POST REQUEST !"})
+    }
+
+}
--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
@@ -7,27 +7,62 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class ConnexionPage : AppCompatActivity()  {
     private lateinit var buttonLogin: Button;
     private lateinit var buttonRegister: Button;
     private lateinit var usernameInput: TextInputEditText;
     private lateinit var passwordInput: TextInputEditText;
+
+    companion object{
+        lateinit var auth: FirebaseAuth
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Fais la connexion
         super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
         setContentView(R.layout.connexion_page);
-        buttonLogin = findViewById(R.id.login_button);
         buttonRegister = findViewById(R.id.register_button);
+        buttonLogin = findViewById(R.id.login_button);
         usernameInput = findViewById(R.id.login_username);
         passwordInput = findViewById(R.id.login_password);
 
         buttonLogin.setOnClickListener({
-            if(usernameInput.text.toString() == "user" && passwordInput.text.toString() == "1234"){
-                Toast.makeText(this, "Login Successfull.", Toast.LENGTH_SHORT).show()
-            } else{
-                Toast.makeText(this, "Login Failed !", Toast.LENGTH_SHORT).show()
+                val email = usernameInput.text.toString();
+                val password = passwordInput.text.toString();
+                if(email.isNotEmpty() && password.isNotEmpty()){
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener({
+                        if(it.isSuccessful){
+                            Toast.makeText(this, "Login in successfully !", Toast.LENGTH_SHORT).show()
 
+                            var intent: Intent = Intent(this,MainActivity::class.java);
+
+                            // Vrai Id d'utilisateur
+                            intent.putExtra("email", email);
+                            startActivity(intent);
+                        }
+                    }).addOnFailureListener({
+                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+
+                    })
+                }
+        })
+
+        buttonRegister.setOnClickListener({
+            val email = usernameInput.text.toString();
+            val password = passwordInput.text.toString();
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener({
+                    if(it.isSuccessful){
+                        Toast.makeText(this, "Account created Successfully !", Toast.LENGTH_SHORT).show()
+                    }
+                }).addOnFailureListener({
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+
+                })
             }
         })
         /*buttonLogin.setOnClickListener{
