fbe1aba7b440e3d651ce93ccc7cc70262ec94a52
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2024-03-28 20:27:24 +0100

    grad

--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ConnexionPage.kt
@@ -2,12 +2,15 @@ package com.example.opimiel_frontend
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
 class ConnexionPage : AppCompatActivity()  {
     private lateinit var buttonLogin: Button;
+    private lateinit var buttonRegister: Button;
     private lateinit var usernameInput: TextInputEditText;
     private lateinit var passwordInput: TextInputEditText;
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +18,18 @@ class ConnexionPage : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.connexion_page);
         buttonLogin = findViewById(R.id.login_button);
+        buttonRegister = findViewById(R.id.register_button);
         usernameInput = findViewById(R.id.login_username);
         passwordInput = findViewById(R.id.login_password);
 
+        buttonLogin.setOnClickListener({
+            if(usernameInput.text.toString() == "user" && passwordInput.text.toString() == "1234"){
+                Toast.makeText(this, "Login Successfull.", Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText(this, "Login Failed !", Toast.LENGTH_SHORT).show()
+
+            }
+        })
         /*buttonLogin.setOnClickListener{
             var intent: Intent = Intent(this,MainActivity::class.java);
 
