0f16911ac628fcec16d2a00dbcbe619ab33dc17a
tawahinBeirut <gheribij59@gmail.com>
2024-03-28 11:22:09 +0100

    Ajout de l'ecran favoris et de l'affichage  en cas d'absence de sujets

--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ui/favorites/FavoritesFragment.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ui/favorites/FavoritesFragment.kt
@@ -1,21 +1,38 @@
 package com.example.opimiel_frontend.ui.favorites
 
+import ApiService
+import OnSubjectClickListener
+import SubjectsResponse
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.opimiel_frontend.HomeAdapter
+import com.example.opimiel_frontend.Subject
 import com.example.opimiel_frontend.databinding.FragmentFavoritesBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
-
+    private lateinit var homeAdapter: HomeAdapter;
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://opimiel.vercel.app/api/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val apiService = retrofit.create(ApiService::class.java)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +49,41 @@ class FavoritesFragment : Fragment() {
         favoritesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Configuration du Home Adaptater
+        with(binding.recyclerViewFavorites) {
+            layoutManager = LinearLayoutManager(context)
+            // On met l'api ici
+
+            adapter = HomeAdapter(requireActivity() as OnSubjectClickListener);
+            homeAdapter = adapter as HomeAdapter;
+        }
+
+
+        apiService.getFavorites((requireActivity() as OnSubjectClickListener).getUserId()).enqueue(object :
+            Callback<SubjectsResponse> {
+            override fun onResponse(call: Call<SubjectsResponse>, response: Response<SubjectsResponse>) {
+                if (response.isSuccessful) {
+                    val body = response.body();
+                    val subjectList: MutableList<Subject> = mutableListOf();
+                    subjectList.clear();
+                    body?.data?.forEach { subject ->
+                        run {
+                            subjectList.add(subject);
+                        }
+                    }
+                    homeAdapter.updateSubjects(subjectList);
+                    Log.d("retussite api",subjectList.toString())
+                } else {
+                    Log.d("Erreur api","oe")
+                }
+            }
+
+            override fun onFailure(call: Call<SubjectsResponse>, t: Throwable) {
+                // Gérer les erreurs de connexion
+                Log.d("Erreur connexion",t.toString())
+            }
+        })
         return root
     }
 
--- opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ui/home/HomeAdaptater.kt
+++ opimielfrontend/app/src/main/java/com/example/opimiel_frontend/ui/home/HomeAdaptater.kt
@@ -11,34 +11,57 @@ import androidx.recyclerview.widget.RecyclerView
 class HomeAdapter(private val listener: OnSubjectClickListener): RecyclerView.Adapter<HomeAdapter.ViewHolder>(){
 
     private var subjectList: MutableList<Subject> = mutableListOf();
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView
+    companion object {
+        private const val TYPE_ITEM = 0
+        private const val TYPE_EMPTY = 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (subjectList.isNullOrEmpty()) TYPE_EMPTY else TYPE_ITEM
+    }
+
+    class ViewHolder(view: View,viewType: Int) : RecyclerView.ViewHolder(view) {
+        lateinit var textView: TextView
         lateinit var participateButton : Button;
+
 
         init {
             // Define click listener for the ViewHolder's View
-            textView = view.findViewById(R.id.text_view_item)
-            participateButton = view.findViewById(R.id.button_subject_switch);
+            if (viewType == TYPE_ITEM) {
+                textView = view.findViewById(R.id.text_view_item)
+                participateButton = view.findViewById(R.id.button_subject_switch);
+            }
+            else{
+                textView = view.findViewById(R.id.text_empty_view)
+                textView.text = "Pas de sujets disponibles"
+            }
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return if (viewType == TYPE_ITEM) {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+            ViewHolder(itemView,viewType)
+        } else {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.empty_view, parent, false)
+            ViewHolder(itemView,viewType)
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item,parent,false);
-        return ViewHolder(itemView)
-    }
-
     override fun getItemCount(): Int {
-        return subjectList.size;
+        return if (subjectList.isEmpty()) 1 else subjectList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentSubject = subjectList[position];
-        holder.textView.text = currentSubject.name;
-        // completer les champs de la view donner
+        if (getItemViewType(position) == TYPE_ITEM) {
+            val currentSubject = subjectList[position];
+            holder.textView.text = currentSubject.name;
+            // completer les champs de la view donner
 
-        holder.participateButton.setOnClickListener {
-            listener.onSubjectClick(currentSubject)
+            holder.itemView.setOnClickListener {
+                listener.onSubjectClick(currentSubject)
+            }
+        }else{
+            holder.textView.text = "Pas de sujets dispos"
         }
     }
     fun updateSubjects(newSubjects: List<Subject>) {
